10a10181d9ba2362478fdda2b9b07a369346ef1a
MartinDixita <Martindixita@gmail.com>
2025-02-20 13:58:54 +0100

    Matematicas

---
+++ tarefa3.py
@@ -0,0 +1,13 @@
+import math
+print (math.sqrt(30))
+pouco = 3
+print (math.sqrt(pouco))
+
+variable1 = 7.4445
+variable2 = 42.223
+variable3 = 10.90
+print (math.sqrt(variable1))
+print (math.sqrt(variable2))
+print (math.sqrt(variable3))
+print (variable3**2)
+print (round(math.pi))
