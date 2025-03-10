dd00820b65eda7878edca38f5fcb51b13cd7de4a
MartinDixita <Martindixita@gmail.com>
2025-03-10 16:49:25 +0100

    Add files via upload

--- tarefa5.py
+++ tarefa5.py
@@ -1,4 +1,4 @@
 contestador = input("Cal é o teu número favorito?")
 numero = int(contestador)
-triple = numero*3
-print("Tres veces do teu número favorito é..." , triple)
+veces = numero*10
+print("dez veces do teu número favorito é..." , veces)
