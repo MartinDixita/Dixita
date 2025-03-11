8ea0f0ac97d62dccf821559603f7ca4d10acd440
MartinDixita <Martindixita@gmail.com>
2025-03-11 13:59:55 +0100

    Add files via upload

---
+++ Ingles.py
@@ -0,0 +1,14 @@
+profesor = input("Como se dice casa en ingles??")
+house = print("Ven contestado")
+
+profesor = input("Como se dice platano en ingles??")
+banana = print("Genial!")
+
+profesor = input("Como se dice basura en ingles??")
+trash = print("Eres moi listo")
+
+profesor = input("Como se dice perro en ingles??")
+dog = print("Ven contestado")
+
+profesor = input("Como se dice gato en ingles??")
+cat = print("Ven contestado")
