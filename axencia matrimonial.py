a2314b9d6f35cf0572e414762584ad69d1f1337c
MartinDixita <Martindixita@gmail.com>
2025-03-17 16:46:20 +0100

    Add files via upload
    Este é o meu programa

---
+++ axencia matrimonial.py
@@ -0,0 +1,13 @@
+print("Benvidos á busca de parellas ideal")
+ollos = input("Cal é o cor de ollos favorito?   ")
+pais = input("Cal é o pais da túa vida?   ")
+deporte = input("Cal é o teu deporte favorito?  ")
+
+if(ollos == "negros"):
+    print("O home da túa vida é Neymar")
+
+if(pais == "Estados Unidos"):
+    print("O home da túa vida é  Michael Jordan")
+
+if(deporte == "Tenis"):
+    print("O home da túa vida é Rafa Nadal")
