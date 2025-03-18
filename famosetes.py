fdb100b54009f1944ceeb4bc7488a81e584c3bfd
MartinDixita <Martindixita@gmail.com>
2025-03-18 13:57:38 +0100

    Add files via upload

---
+++ famosetes.py
@@ -0,0 +1,20 @@
+print("Benvidos á buscador")
+
+#ollos = input("Cal é o cor de ollos do teu famoso?   ")
+
+pais = input("Cal é o pais da persona na que pensas?   ")
+
+deporte = input("Cal é o seu deporte?  ")
+
+
+
+#if(ollos == "azules"):
+ #    print("Estas pensando en Donald Trump")
+ 
+if(pais == "España"):
+    if(deporte == "badminton"):
+        print("Estas pensando en Carolina Marín")
+
+    if(deporte == "atletismo"):
+        print("Estas pensando en Ana Peleteiro")
+
